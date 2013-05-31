c44de2b77fdc1149fe02e6962293d27071753584
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2013-05-31 12:36:03 +0200

    remove IE -> Ie special case

--- pytest_mozwebqa/selenium_client.py
+++ pytest_mozwebqa/selenium_client.py
@@ -125,8 +125,6 @@ class Client(object):
                 firefox_binary=binary,
                 firefox_profile=profile,
                 capabilities=capabilities or None)
-        elif self.driver.upper() == 'IE':
-            self.selenium = webdriver.Ie()
         elif self.driver.upper() == 'OPERA':
             capabilities.update(webdriver.DesiredCapabilities.OPERA)
             self.selenium = webdriver.Opera(executable_path=self.opera_path,
