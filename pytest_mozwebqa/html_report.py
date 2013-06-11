272a40f7d7ac71c10a5d19a0711db58b7ad13ae5
Ronny Pfannschmidt <Ronny.Pfannschmidt@gmx.de>
2013-06-11 11:47:37 +0200

    decompose html report composition into functions

--- pytest_mozwebqa/html_report.py
+++ pytest_mozwebqa/html_report.py
@@ -5,6 +5,7 @@
 # file, You can obtain one at http://mozilla.org/MPL/2.0/.
 
 import cgi
+import codecs
 import datetime
 import os
 
@@ -13,7 +14,6 @@ import py
 import time
 from base64 import decodestring as decode_b64
 
-
 from py.xml import html
 from py.xml import raw
 
@@ -21,7 +21,6 @@ import sauce_labs
 
 
 class HTMLReport(object):
-
     def __init__(self, config):
         logfile = os.path.expanduser(os.path.expandvars(config.option.webqa_report_path))
         self.logfile = py.path.local(logfile)
@@ -67,7 +66,7 @@ class HTMLReport(object):
 
             if 'screenshots' in debug:
                 add_file('Screenshot', 'screenshot.png',
-                        decode_b64(debug['screenshots']))
+                         decode_b64(debug['screenshots']))
 
             if 'html' in debug:
                 add_file('HTML', 'html.txt', debug['html'])
@@ -75,7 +74,7 @@ class HTMLReport(object):
             # Log may contain passwords, etc so we only capture it for tests marked as public
             if 'logs' in debug:
                 if 'public' in report.keywords:
-                    add_file('Log','log.txt', debug['logs'])
+                    add_file('Log', 'log.txt', debug['logs'])
                 else:
                     add_link('Log not public', '#')
 
@@ -124,12 +123,13 @@ class HTMLReport(object):
                 additional_html.append(log)
 
         self.test_logs.append(html.tr([
-            html.td(result, class_='col-result'),
-            html.td(testclass, class_='col-class'),
-            html.td(testmethod, class_='col-name'),
-            html.td(round(time), class_='col-duration'),
-            html.td(links_html, class_='col-links'),
-            html.td(additional_html, class_='debug')], class_=result.lower() + ' results-table-row'))
+                                          html.td(result, class_='col-result'),
+                                          html.td(testclass, class_='col-class'),
+                                          html.td(testmethod, class_='col-name'),
+                                          html.td(round(time), class_='col-duration'),
+                                          html.td(links_html, class_='col-links'),
+                                          html.td(additional_html, class_='debug')],
+                                      class_=result.lower() + ' results-table-row'))
 
     def _make_report_dir(self):
         logdir = self.logfile.dirpath()
@@ -178,33 +178,10 @@ class HTMLReport(object):
 
     def pytest_sessionfinish(self, session, exitstatus, __multicall__):
         self._make_report_dir()
-        logfile = py.std.codecs.open(str(self.logfile), 'w', encoding='utf-8')
 
         suite_stop_time = time.time()
         suite_time_delta = suite_stop_time - self.suite_start_time
-        numtests = sum(self.counts.values())
 
-        option = self.config.option
-
-        server = option.sauce_labs_credentials_file and \
-                 'Sauce Labs' or 'http://%s:%s' % (option.host, option.port)
-        browser = '%s %s on %s' % (str(option.browser_name).title(),
-                                   option.browser_version,
-                                   str(option.platform).title())
-        generated = datetime.datetime.now()
-        configuration = {
-            'Base URL': option.base_url,
-            'Build': option.build,
-            'Driver': option.driver,
-            'Firefox Path': option.firefox_path,
-            'Google Chrome Path': option.chrome_path,
-            'Selenium Server': server,
-            'Browser': browser,
-            'Timeout': option.webqatimeout,
-            'Credentials': option.credentials_file,
-            'Sauce Labs Credentials': option.sauce_labs_credentials_file}
-
-        import pytest_mozwebqa
         doc = html.html(
             html.head(
                 html.meta(charset='utf-8'),
@@ -213,34 +190,89 @@ class HTMLReport(object):
                 html.script(src='jquery.js'),
                 html.script(src='main.js')),
             html.body(
-                html.p('Report generated on %s at %s by pytest-mozwebqa %s' % (
-                    generated.strftime('%d-%b-%Y'),
-                    generated.strftime('%H:%M:%S'),
-                    pytest_mozwebqa.__version__)),
-                html.h2('Configuration'),
-                html.table(
-                    [html.tr(html.td(k), html.td(v)) for k, v in sorted(configuration.items()) if v],
-                    id='configuration'),
-                html.h2('Summary'),
-                html.p(
-                    '%i tests ran in %i seconds.' % (numtests, suite_time_delta),
-                    html.br(),
-                    html.span('%(passed)i passed' % self.counts, class_='passed'), ', ',
-                    html.span('%(skipped)i skipped' % self.counts, class_='skipped'), ', ',
-                    html.span('%(failed)i failed' % self.counts, class_='failed'), ', ',
-                    html.span('%(error)i errors' % self.counts, class_='error'), '.',
-                    html.br(),
-                    html.span('%(xpassed)i expected failures' % self.counts, class_='skipped'), ', ',
-                    html.span('%(xfailed)i unexpected passes' % self.counts, class_='failed'), '.'),
-                html.h2('Results'),
-                html.table([
-                    html.thead(html.tr([
-                        html.th('Result', class_='sortable', col='result'),
-                        html.th('Class', class_='sortable', col='class'),
-                        html.th('Name', class_='sortable', col='name'),
-                        html.th('Duration', class_='sortable numeric', col='duration'),
-                        html.th('Links')]), id='results-table-head'),
-                    html.tbody(self.test_logs, id='results-table-body')], id='results-table')))
+                _reportheader(),
+                _configuration(self.config.option),
+                _summary(self.counts, suite_time_delta),
+                _result_table(self.test_logs)),
+        )
+        with codecs.open(str(self.logfile), 'w', encoding='utf-8') as fp:
+            fp.write(u'<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">')
+            fp.write(doc.unicode(indent=2))
 
-        logfile.write('<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">' + doc.unicode(indent=2))
-        logfile.close()
+
+def _reportheader():
+    generated = datetime.datetime.now()
+    import pytest_mozwebqa
+
+    return html.p(
+        'Report generated on ',
+        format(generated, '%d-%b-%Y at %H:%M:%S'),
+        ' by pytest-mozwebqa ',
+        pytest_mozwebqa.__version__)
+
+
+def _configuration(option):
+    server = option.sauce_labs_credentials_file and \
+             'Sauce Labs' or 'http://%s:%s' % (option.host, option.port)
+    browser = '%s %s on %s' % (
+        str(option.browser_name).title(),
+        option.browser_version,
+        str(option.platform).title())
+
+    configuration = {
+        'Base URL': option.base_url,
+        'Build': option.build,
+        'Driver': option.driver,
+        'Firefox Path': option.firefox_path,
+        'Google Chrome Path': option.chrome_path,
+        'Selenium Server': server,
+        'Browser': browser,
+        'Timeout': option.webqatimeout,
+        'Credentials': option.credentials_file,
+        'Sauce Labs Credentials': option.sauce_labs_credentials_file,
+    }
+
+    return [
+        html.h2('Configuration'),
+        html.table(
+            [html.tr(html.td(k), html.td(v))
+             for k, v in sorted(configuration.items()) if v],
+            id='configuration'),
+    ]
+
+
+def _summary(counts, time_delta):
+    numtests = sum(counts.values())
+
+    return [
+        html.h2('Summary'),
+        html.p(
+            '%i tests ran in %i seconds.' % (numtests, time_delta),
+            html.br(),
+            html.span('%(passed)i passed' % counts, class_='passed'), ', ',
+            html.span('%(skipped)i skipped' % counts, class_='skipped'), ', ',
+            html.span('%(failed)i failed' % counts, class_='failed'), ', ',
+            html.span('%(error)i errors' % counts, class_='error'), '.',
+            html.br(),
+            html.span('%(xpassed)i expected failures' % counts, class_='skipped'), ', ',
+            html.span('%(xfailed)i unexpected passes' % counts, class_='failed'), '.'),
+    ]
+
+
+def _result_table(logs):
+    return [
+        html.h2('Results'),
+        html.table(
+            html.thead(
+                html.tr(
+                    html.th('Result', class_='sortable', col='result'),
+                    html.th('Class', class_='sortable', col='class'),
+                    html.th('Name', class_='sortable', col='name'),
+                    html.th('Duration', class_='sortable numeric', col='duration'),
+                    html.th('Links')),
+                id='results-table-head'),
+            html.tbody(logs, id='results-table-body'),
+            id='results-table')
+    ]
+
+
